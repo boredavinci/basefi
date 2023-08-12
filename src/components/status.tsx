9f5a58db48a72a3c60e2f6b4585262402755cd82
@boredavinci <boredavinci@gmail.com>
2023-08-12 18:11:19 +0100

    Change to status

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -5,7 +5,7 @@ export default function Status() {
     <div>
       <span className='inline-flex items-center bg-green-100/70 text-green-800 text-sm font-bold mr-2 px-2 py-1 rounded-full'>
         <span className='w-3 h-3 mr-2 bg-green-500 rounded-full'></span>
-        Funding
+        Open
       </span>
       {/* <span className='inline-flex items-center bg-yellow-100/70 text-yellow-800 text-sm font-bold mr-2 px-2 py-1 rounded-full'>
         <span className='w-3 h-3 mr-2 bg-yellow-500 rounded-full'></span>
