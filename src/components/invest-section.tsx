29cf68935cb02097517d03fea66de514d4971cb7
@boredavinci <boredavinci@gmail.com>
2023-08-12 22:05:25 +0100

    Updated input styling

--- src/components/invest-section.tsx
+++ src/components/invest-section.tsx
@@ -12,13 +12,16 @@ export default function InvestNow() {
             <p className='mt-1 text-3xl '>60%</p>
           </div>
         </div>
-        <div className='flex h-20 w-full px-4 rounded-full border-4 border-primary-100 hover:border-primary-200 shadow-[inset_0_-5px_15px_rgba(0,0,256,0.3)] '>
-          <input className='w-full mr-4 bg-transparent' type={'number'}></input>
+        <div className='flex h-20 w-full px-6 rounded-full border-4 border-primary-100 hover:border-primary-200 shadow-[inset_0_-5px_15px_rgba(0,0,256,0.3)] '>
+          <input
+            className='w-full mr-4 px-2 bg-transparent'
+            type={'number'}
+          ></input>
           <button className='text-primary hover:text-primary-700 font-semibold'>
             max
           </button>
         </div>
-        <button className='mt-4 bg-primary h-12  w-full hover:bg-primary-600 text-white py-2 text-center rounded-full'>
+        <button className='mt-4 bg-primary h-12 w-full hover:bg-primary-600 text-white py-2 text-center rounded-full'>
           <p>Invest Now</p>
         </button>
       </div>
@@ -29,8 +32,11 @@ export default function InvestNow() {
             <p className='mt-1 text-3xl'>+16%</p>
           </div>
         </div>
-        <div className='flex h-20 w-full px-4 rounded-full border-4 border-primary-100 hover:border-primary-200 shadow-[inset_0_-5px_15px_rgba(0,0,256,0.3)] '>
-          <input className='w-full mr-4 bg-transparent' type={'number'}></input>
+        <div className='flex h-20 w-full px-6 rounded-full border-4 border-primary-100 hover:border-primary-200 shadow-[inset_0_-5px_15px_rgba(0,0,256,0.3)] '>
+          <input
+            className='w-full mr-4 px-2 bg-transparent'
+            type={'number'}
+          ></input>
           <button className='text-primary hover:text-primary-700 font-semibold'>
             max
           </button>
