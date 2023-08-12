1c3d9b99f8b8027646673471735fd9983bab5aea
@boredavinci <boredavinci@gmail.com>
2023-08-12 17:50:49 +0100

    Added closed  and exited status in comments

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -7,6 +7,14 @@ export default function Status() {
         <span className='w-3 h-3 mr-2 bg-green-500 rounded-full'></span>
         Funding
       </span>
+      {/* <span className='inline-flex items-center bg-yellow-100/70 text-yellow-800 text-sm font-bold mr-2 px-2 py-1 rounded-full'>
+        <span className='w-3 h-3 mr-2 bg-yellow-500 rounded-full'></span>
+        Closed
+      </span>
+      <span className='inline-flex items-center bg-slate-100/70 text-slate-800 text-sm font-bold mr-2 px-2 py-1 rounded-full'>
+        <span className='w-3 h-3 mr-2 bg-slate-500 rounded-full'></span>
+        Exited
+      </span> */}
     </div>
   );
 }
