60ab7fa252e4dd1aa972749cdad14147cabc456b
@boredavinci <boredavinci@gmail.com>
2023-08-12 02:36:34 +0100

    Updated deal cards

--- src/components/dealcard.tsx
+++ src/components/dealcard.tsx
@@ -3,51 +3,57 @@ import Link from 'next/link';
 
 const deals = [
   {
-    name: 'Innovation fund',
-    closing: '25 days',
+    name: ' Birth of venus',
+    artist: 'Botticelli',
     tvl: '32,000,000',
+    juniorapy: '28%',
     seniorapy: '5%',
-    logoUrl: 'https://fakeimg.pl/200x200/0052FF/fff?text=logo&font=museo',
+    logoUrl: 'https://fakeimg.pl/800x800/0052FF/fff?text=art&font=museo',
     url: 'innovation-fund ',
   },
   {
-    name: 'Innovation fund',
-    closing: '25 days',
+    name: ' Birth of venus',
+    artist: 'Botticelli',
     tvl: '32,000,000',
+    juniorapy: '28%',
     seniorapy: '5%',
-    logoUrl: 'https://fakeimg.pl/200x200/0052FF/fff?text=logo&font=museo',
+    logoUrl: 'https://fakeimg.pl/800x800/0052FF/fff?text=art&font=museo',
     url: 'innovation-fund ',
   },
   {
-    name: 'Innovation fund',
-    closing: '25 days',
+    name: ' Birth of venus',
+    artist: 'Botticelli',
     tvl: '32,000,000',
+    juniorapy: '28%',
     seniorapy: '5%',
-    logoUrl: 'https://fakeimg.pl/200x200/0052FF/fff?text=logo&font=museo',
+    logoUrl: 'https://fakeimg.pl/800x800/0052FF/fff?text=art&font=museo',
     url: 'innovation-fund ',
   },
   {
-    name: 'Innovation fund',
-    closing: '25 days',
+    name: ' Birth of venus',
+    artist: 'Botticelli',
     tvl: '32,000,000',
+    juniorapy: '28%',
     seniorapy: '5%',
-    logoUrl: 'https://fakeimg.pl/200x200/0052FF/fff?text=logo&font=museo',
+    logoUrl: 'https://fakeimg.pl/800x800/0052FF/fff?text=art&font=museo',
     url: 'innovation-fund ',
   },
   {
-    name: 'Innovation fund',
-    closing: '25 days',
+    name: ' Birth of venus',
+    artist: 'Botticelli',
     tvl: '32,000,000',
+    juniorapy: '28%',
     seniorapy: '5%',
-    logoUrl: 'https://fakeimg.pl/200x200/0052FF/fff?text=logo&font=museo',
+    logoUrl: 'https://fakeimg.pl/800x800/0052FF/fff?text=art&font=museo',
     url: 'innovation-fund ',
   },
   {
-    name: 'Innovation fund',
-    closing: '25 days',
+    name: ' Birth of venus',
+    artist: 'Botticelli',
     tvl: '32,000,000',
+    juniorapy: '28%',
     seniorapy: '5%',
-    logoUrl: 'https://fakeimg.pl/200x200/0052FF/fff?text=logo&font=museo',
+    logoUrl: 'https://fakeimg.pl/800x800/0052FF/fff?text=art&font=museo',
     url: 'innovation-fund ',
   },
 ];
@@ -56,7 +62,7 @@ export default function DealCard() {
   return (
     <ul
       role='list'
-      className='mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8 text-center'
+      className='mx-auto mt-12 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-8 text-center'
     >
       {deals.map((deal) => (
         <Link
@@ -65,22 +71,21 @@ export default function DealCard() {
           href={deal.url}
         >
           <Image
-            className='mx-auto h-24 w-24 rounded-full '
+            className='mx-auto w-full rounded-3xl'
             src={deal.logoUrl}
-            width={200}
-            height={200}
+            width={300}
+            height={300}
             alt=''
           />
           <h2 className='mt-6 text-2xl'>{deal.name}</h2>
-          <h3 className='mt-6 text-xl'>{deal.closing} till close</h3>
           <div className='flex  mt-6 pt-4 justify-between'>
-            <div className=' text-left '>
-              <p className='text-sm uppercase font-semibold '>Senior APY</p>
-              <p className=' text-lg font-bold'>{deal.seniorapy}</p>
+            <div className='text-left uppercase'>
+              <p className='text-sm'>Senior APY</p>
+              <p className='mt-1 text-2xl'>{deal.seniorapy}</p>
             </div>
             <div className=' text-left'>
-              <p className='text-sm uppercase font-semibold'>TVL</p>
-              <p className=' text-lg font-bold'>{deal.tvl}</p>
+              <p className='text-xs '>Junior APY</p>
+              <p className='mt-1 text-3xl '>{deal.juniorapy}</p>
             </div>
           </div>
         </Link>
