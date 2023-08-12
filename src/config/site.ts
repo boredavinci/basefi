957483186b953fbbdd72c6a805edc75695e52f3b
@boredavinci <boredavinci@gmail.com>
2023-08-12 02:30:00 +0100

    updated siteconfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,9 @@ import { SiteConfig } from '@/types';
 
 export const siteConfig: SiteConfig = {
   name: 'BaseFI',
-  description: 'Decentralized Lending on Base',
+  description: 'Invest in real world masterpieces',
+  keyword:
+    'alternative investments, alts, investments, art investment, masterpiece investments',
   ogImage: '',
   links: {
     github: '',
