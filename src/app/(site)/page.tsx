b20c6a6890d71ecc22acb39f843123f02cc63889
@boredavinci <boredavinci@gmail.com>
2023-08-12 02:34:02 +0100

    Updated homepage

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,15 +3,10 @@ import { siteConfig } from '@/config/site';
 
 export default function Home() {
   return (
-    <main className='flex flex-col  '>
-      <section className='space-y-6 '>
-        <div className='text-center'>
-          <h1 className=' text-5xl lg:text-6xl  mt-20'>
-            {siteConfig.description}
-          </h1>
-          <DealCard />
-        </div>
-      </section>
+    <main className='flex flex-col'>
+      <h1 className='mx-auto text-5xl lg:text-6xl mt-24'>
+        {siteConfig.description}
+      </h1>
     </main>
   );
 }
