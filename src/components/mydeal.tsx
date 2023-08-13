a0b07f84520b166a033ecd603c8a0afe7c71cc92
Bruno Wu <wubruno@outlook.com>
2023-08-13 16:14:31 +0200

    Calculate returns

--- src/components/mydeal.tsx
+++ src/components/mydeal.tsx
@@ -1,46 +1,63 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { formatEther } from 'viem';
 
 import Status from '@/components/status';
-import { FundInvestments, FundStage, FundsDeployed } from '@/service/fund';
-
-const deals = [
-  {
-    name: ' Water Lily Pond',
-    artist: 'Claude Monet',
-    date: 'June 2016',
-    juniorapy: '18,729',
-    seniorapy: '17,000',
-    logoUrl: '/water-lily-pond-harmony-in-green.jpg',
-    url: 'water-lily-pond-harmony-in-green',
-  },
-  {
-    name: 'Bal du moulin',
-    artist: 'Pierre Renoir',
-    date: 'March 2017',
-    juniorapy: '13,212',
-    seniorapy: '12,000',
-    logoUrl: '/bal-du-moulin.jpg',
-    url: 'bal-du-moulin',
-  },
-];
+import {
+  FundInvestments,
+  FundStage,
+  FundsDeployed,
+  calculateReturns,
+  deals,
+  formatBytes32,
+  getFundDeployed,
+  parseBytes32,
+} from '@/service/fund';
 
 export default function MyDeal({
   fundInvestments,
+  fundsDeployed,
 }: {
   fundInvestments?: FundInvestments;
   fundsDeployed?: FundsDeployed;
 }) {
+  const investmentTargets =
+    fundsDeployed &&
+    [
+      ...new Set(
+        fundInvestments?.map((fundInvestment) =>
+          formatBytes32(fundInvestment.symbol!)
+        )
+      ),
+    ].map((symbol) => {
+      const fund = getFundDeployed(fundsDeployed, symbol);
+      const total = fundInvestments
+        ?.filter((invest) => formatBytes32(invest.symbol!) == symbol)
+        .reduce((acc, x) => acc + x.amount!, 0n);
+      const returned = fundInvestments
+        ?.filter((invest) => formatBytes32(invest.symbol!) == symbol)
+        .map((fund) => calculateReturns(fund, fundsDeployed))
+        .reduce((acc, x) => acc! + x!, 0n);
+
+      return {
+        ...fund,
+        total,
+        returned,
+      };
+    });
+
+  console.log(investmentTargets);
+
   return (
     <ul
       role='list'
       className='mx-auto grid max-w-2xl grid-cols-1 gap-6  sm:grid-cols-2 lg:grid-cols-3 lg:mx-0 lg:max-w-none lg:gap-8 text-center grid-flow-row-dense'
     >
-      {deals.map((deal) => (
+      {investmentTargets?.map((deal) => (
         <Link
           key={deal.name}
           className='rounded-3xl px-6 py-8 shadow-2xl  shadow-fuchsia-200 h-fit card relative'
-          href={deal.url}
+          href={`/${deal.symbol}`}
         >
           <div className='absolute shadow-2xl top-10 right-6'>
             <Status stage={FundStage.FUNDING} />
@@ -48,20 +65,22 @@ export default function MyDeal({
 
           <Image
             className='mx-auto w-full rounded-3xl 24 h-60 object-cover'
-            src={deal.logoUrl}
+            src={(deal.symbol && deals.get(deal.symbol)?.logoUrl) || ''}
             width={400}
             height={400}
-            alt={deal.name}
+            alt={deal.symbol || ''}
           />
           <h2 className='mt-6 text-2xl'>{deal.name}</h2>
           <div className='flex pt-4 justify-between'>
             <div className='text-left uppercase'>
               <p className='text-xs'>Invested</p>
-              <p className='mt-1 text-2xl'>{deal.seniorapy}</p>
+              <p className='mt-1 text-2xl'>{formatEther(deal.total || 0n)}</p>
             </div>
             <div className=' text-left uppercase'>
-              <p className='text-xs '>Current value</p>
-              <p className='mt-1 text-2xl '>{deal.juniorapy}</p>
+              <p className='text-xs '>Expected Return</p>
+              <p className='mt-1 text-2xl '>
+                {formatEther(deal.returned || 0n)}
+              </p>
             </div>
           </div>
         </Link>
